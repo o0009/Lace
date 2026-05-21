ff905dda4e80a32257eb02978fa0534ab786ec4d
o0009 <84551915+o0009@users.noreply.github.com>
2026-05-21 14:20:20 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Let's jump Alexis",
+        name: "Find The Kpop Idol",
         // Activity type number (0 = Playing).
         type: 0, 
       },
